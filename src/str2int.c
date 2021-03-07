0aed75268bffd62200e44607e592742cbdbb6b19
Gyps <N/A>
2021-03-07 15:48:00 +0100

    Initial content added.

--- src/str2int.c
+++ src/str2int.c
@@ -6,3 +6,64 @@
  */
 
 #include "str2int.h"
+#include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include <ctype.h>
+
+
+
+//---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * C-String zu long int konvertieren.
+ */
+enum str2int_errno                          // Error-enum (STR2INT_SUCCESS: Erfolgreich durchgefuehrt)
+str2int
+(
+        long int* restrict out,             // Ausgabeadresse
+        const char* restrict input_string,  // String, der konvertiert werden soll
+        const int base                      // Zahlensystem
+)
+{
+    char* end = NULL;
+
+    if (input_string == NULL)
+    {
+        return STR2INT_INCONVERTIBLE;
+    }
+    if (input_string [0] == '\0' || isspace (input_string [0]))
+    {
+        return STR2INT_INCONVERTIBLE;
+    }
+    if (base <= 0)
+    {
+        return STR2INT_INCONVERTIBLE;
+    }
+    if (out == NULL)
+    {
+        return STR2INT_INCONVERTIBLE;
+    }
+
+    errno = 0;
+    const long int l = strtol (input_string, &end, base);
+
+    if (l > LONG_MAX || (errno == ERANGE && l == LONG_MAX))
+    {
+        return STR2INT_OVERFLOW;
+    }
+    if (l < LONG_MIN || (errno == ERANGE && l == LONG_MIN))
+    {
+        return STR2INT_UNDERFLOW;
+    }
+    if (*end != '\0')
+    {
+        return STR2INT_INCONVERTIBLE;
+    }
+
+    *out = l;
+
+    return STR2INT_SUCCESS;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
--- src/str2int.h
+++ src/str2int.h
@@ -8,6 +8,32 @@
 #ifndef STR2INT_H
 #define STR2INT_H
 
+// BEGINN C++-Kompablitaet herstellen
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
 
+
+
+enum str2int_errno
+{
+    STR2INT_SUCCESS = 0,
+    STR2INT_OVERFLOW,
+    STR2INT_UNDERFLOW,
+    STR2INT_INCONVERTIBLE
+};
+
+/**
+ * C-String zu long int konvertieren.
+ */
+extern enum str2int_errno str2int (long int* out, const char* input_string, const int base);
+
+
+
+// ENDE C++-Kompablitaet herstellen
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* STR2INT_H */
