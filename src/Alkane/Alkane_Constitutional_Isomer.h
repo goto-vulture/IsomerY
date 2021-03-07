578dd1f865bec484ca4e2f22c7910937578da0c9
Gyps <N/A>
2021-03-07 22:04:43 +0100

    Alkane_Constitutional_Isomer.h created.

---
+++ src/Alkane/Alkane_Constitutional_Isomer.h
@@ -0,0 +1,13 @@
+/**
+ * Alkane_Constitutional_Isomer.h
+ *
+ *  Created on: 07.03.2021
+ *      Author: x86
+ */
+
+#ifndef ALKANE_ALKANE_CONSTITUTIONAL_ISOMER_H
+#define ALKANE_ALKANE_CONSTITUTIONAL_ISOMER_H
+
+
+
+#endif /* ALKANE_ALKANE_CONSTITUTIONAL_ISOMER_H */
