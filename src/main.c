14de86380a7c0a35fdf128567f2e629a2c17e2d8
Gyps <N/A>
2021-03-07 16:09:10 +0100

    static_asserts added.

--- src/main.c
+++ src/main.c
@@ -5,7 +5,14 @@
  *      Author: x86
  */
 
+#include <assert.h>
+#include <limits.h>
 #include "Error_Handling/Dynamic_Memory.h"
+
+
+
+static_assert (sizeof (int) == 4, "sizeof (int) needs to be exact 4 byte !");
+static_assert (CHAR_BIT == 8, "A byte needs to be exact 8 bit !");
 
 
 
