5cd31f55c3d4f0eb110f7ca7c79d11032a8decb1
Gyps <N/A>
2021-03-07 15:51:55 +0100

    Assert_Msg.h created.

---
+++ src/Error_Handling/Assert_Msg.h
@@ -0,0 +1,13 @@
+/**
+ * Assert_Msg.h
+ *
+ *  Created on: 07.03.2021
+ *      Author: x86
+ */
+
+#ifndef ERROR_HANDLING_ASSERT_MSG_H
+#define ERROR_HANDLING_ASSERT_MSG_H
+
+
+
+#endif /* ERROR_HANDLING_ASSERT_MSG_H */
