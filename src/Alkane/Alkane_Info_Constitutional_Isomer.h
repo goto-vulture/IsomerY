f74d39ac0f9efc919d6b5ad2cc28a658041a4d92
Gyps <N/A>
2021-03-07 22:18:03 +0100

    Initial content added.

--- src/Alkane/Alkane_Info_Constitutional_Isomer.h
+++ src/Alkane/Alkane_Info_Constitutional_Isomer.h
@@ -8,6 +8,33 @@
 #ifndef ALKANE_ALKANE_CONSTITUTIONAL_ISOMER_H
 #define ALKANE_ALKANE_CONSTITUTIONAL_ISOMER_H
 
+// BEGINN C++-Kompablitaet herstellen
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
 
+
+#ifndef MAX_NUMBER_OF_C_ATOMS
+    #define MAX_NUMBER_OF_C_ATOMS 40
+#else
+    #error "The macro \"MAX_NUMBER_OF_C_ATOMS\" is already defined !"
+#endif /* MAX_NUMBER_OF_C_ATOMS */
+
+
+
+#include <inttypes.h>
+
+
+
+// Anzahl der Konsitutionsisomere bei gegebener Anzahl an C-Atomen
+extern const uint_fast64_t NUMBER_OF_ALKANE_CONSTITUTIONAL_ISOMER [MAX_NUMBER_OF_C_ATOMS];
+
+
+
+// ENDE C++-Kompablitaet herstellen
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* ALKANE_ALKANE_CONSTITUTIONAL_ISOMER_H */
