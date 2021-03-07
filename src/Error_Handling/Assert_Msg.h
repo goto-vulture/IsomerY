b1b01ec7efe5bd40c9003d2c82c7db76f78ee239
Gyps <N/A>
2021-03-07 16:07:59 +0100

    Initial content added.

--- src/Error_Handling/Assert_Msg.h
+++ src/Error_Handling/Assert_Msg.h
@@ -8,6 +8,32 @@
 #ifndef ERROR_HANDLING_ASSERT_MSG_H
 #define ERROR_HANDLING_ASSERT_MSG_H
 
+// BEGINN C++-Kompablitaet herstellen
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
 
+
+
+#include <assert.h>
+
+
+
+/**
+ * Ein Assert mit benutzerdefinierter Fehlermeldung.
+ */
+#ifndef ASSERT_MSG
+    #define ASSERT_MSG(expr, message) assert (((void) (message), expr));
+#else
+    #error "The macro \"ASSERT_MSG\" is already defined !"
+#endif /* ASSERT_MSG */
+
+
+
+// ENDE C++-Kompablitaet herstellen
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* ERROR_HANDLING_ASSERT_MSG_H */
