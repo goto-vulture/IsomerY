8023aa40ea088fda3f44ec0f7250b5ece6f5919c
Gyps <N/A>
2021-03-08 21:08:30 +0100

    Alkane_Branch.h created.

---
+++ src/Alkane/Alkane_Branch.h
@@ -0,0 +1,13 @@
+/**
+ * Alkane_Branch.h
+ *
+ *  Created on: 08.03.2021
+ *      Author: x86 / Gyps
+ */
+
+#ifndef ALKANE_ALKANE_BRANCH_H
+#define ALKANE_ALKANE_BRANCH_H
+
+
+
+#endif /* ALKANE_ALKANE_BRANCH_H */
