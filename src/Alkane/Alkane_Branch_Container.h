c18ce7d630177f07ca7071ca0b42deac90c5e8c6
Gyps <N/A>
2021-03-09 17:52:51 +0100

    Alkane_Branch_Container.h created.

---
+++ src/Alkane/Alkane_Branch_Container.h
@@ -0,0 +1,13 @@
+/**
+ * Alkane_Branch_Container.h
+ *
+ *  Created on: 09.03.2021
+ *      Author: x86 / Gyps
+ */
+
+#ifndef ALKANE_BRANCH_CONTAINER_H
+#define ALKANE_BRANCH_CONTAINER_H
+
+
+
+#endif /* ALKANE_ALKANE_BRANCH_CONTAINER_H */
