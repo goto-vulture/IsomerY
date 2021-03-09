19073257593ab37ffc82f4dd3072cbea4b27285d
Gyps <N/A>
2021-03-09 14:13:01 +0100

    A new assert macro was added. It is used for allocation checks.

--- src/Error_Handling/Assert_Msg.h
+++ src/Error_Handling/Assert_Msg.h
@@ -17,6 +17,8 @@ extern "C"
 
 
 #include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 
 
@@ -28,6 +30,22 @@ extern "C"
 #else
     #error "The macro \"ASSERT_MSG\" is already defined !"
 #endif /* ASSERT_MSG */
+
+/**
+ * Ein Assert fuer Fehlerfaelle bei der Allokation von Speicher.
+ * Vor der eigentlichen Assert-Meldung wird die Groesse des Speichers angegeben, der allokiert werden sollte.
+ */
+#ifndef ASSERT_ALLOC
+    #define ASSERT_ALLOC(ptr, message, memory_size)                     \
+    if (ptr == NULL)                                                    \
+    {                                                                   \
+        fprintf (stderr, "Try to allocate %zu bytes !\n", memory_size); \
+        fflush (stderr);                                                \
+        ASSERT_MSG(false, message)                                      \
+    }
+#else
+    #error "The macro \"ASSERT_ALLOC\" is already defined !"
+#endif /* ASSERT_ALLOC */
 
 
 
