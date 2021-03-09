2bf05ef52a1898a02c385a3f4014ae1990e068c4
Gyps <N/A>
2021-03-09 15:49:26 +0100

    Semicolons after macros were added. This makes the development with many editors easier.

--- src/Alkane/Alkane_Branch.c
+++ src/Alkane/Alkane_Branch.c
@@ -38,13 +38,13 @@ Create_Alkane_Branch
     // Wenn Astinformationen vorhanden sind, dann muss die Laengenangabe ueberprueft werden
     if (branch != NULL)
     {
-        ASSERT_MSG(length > 0, "The length for the branch values is 0 !")
-        ASSERT_MSG(length <= (MAX_NUMBER_OF_C_ATOMS + 1) / 2, "The length value is too large !")
+        ASSERT_MSG(length > 0, "The length for the branch values is 0 !");
+        ASSERT_MSG(length <= (MAX_NUMBER_OF_C_ATOMS + 1) / 2, "The length value is too large !");
     }
 
     // Neues Objekt anlegen und Allokation ueberpruefen
     struct Alkane_Branch* new_branch = (struct Alkane_Branch*) CALLOC (1, sizeof (struct Alkane_Branch));
-    ASSERT_ALLOC(new_branch, "Cannot allocate an Alkane_Branch object !", sizeof (struct Alkane_Branch))
+    ASSERT_ALLOC(new_branch, "Cannot allocate an Alkane_Branch object !", sizeof (struct Alkane_Branch));
 
     // Neues Objekt mit den uebergebenen Astinformationen initialisieren, falls vorhanden
     if (branch != NULL)
@@ -210,10 +210,10 @@ Delete_Alkane_Branch
         struct Alkane_Branch* restrict branch   // Alkane_Branch, welches geloescht werden soll
 )
 {
-    ASSERT_MSG(branch != NULL, "Branch is NULL !")
+    ASSERT_MSG(branch != NULL, "Branch is NULL !");
 
     branch->state = ALKANE_BRANCH_DELETED;
-    FREE_AND_SET_TO_NULL(branch)
+    FREE_AND_SET_TO_NULL(branch);
 
     return;
 }
