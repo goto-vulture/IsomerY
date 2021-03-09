d31eccd298b391d4bd0f178418091a6fec03754f
Gyps <N/A>
2021-03-09 15:33:55 +0100

    Alkane_Branch_To_String () implemented.

--- src/Alkane/Alkane_Branch.c
+++ src/Alkane/Alkane_Branch.c
@@ -66,6 +66,110 @@ Create_Alkane_Branch
 //---------------------------------------------------------------------------------------------------------------------
 
 /**
+ * Alkane_Branch Objekt in eine Zeichenkettendarstellung ueberfuehren. Dies ist insbesondere fuer debugging hilfreich.
+ *
+ * Asserts:
+ *      branch != NULL,
+ *      string_memory != NULL,
+ *      string_memory_size > 0
+ */
+const char*                                         // Adresse der Eingabe-Zeichenkette
+Alkane_Branch_To_String
+(
+        const struct Alkane_Branch* restrict const branch,
+                                                    // Alkane_Branch, der als Zeichenkette dargestellt werden soll
+        char* const restrict string_memory,         // Speicher, der fuer die Zeichenkettenerzeugung verwendet werden
+                                                    // soll
+                                                    // Der Speicher muss vorher vom Aufrufer allokiert werden
+        const size_t string_memory_size             // Groesse des Zeichenkettenspeichers
+                                                    // Wenn der Speicher nicht ausreicht, dann wird am vorletzten
+                                                    // Zeichen die Zeichenkette abgeschnitten
+)
+{
+    ASSERT_MSG(branch != NULL, "branch is NULL !");
+    ASSERT_MSG(string_memory != NULL, "string_memory is NULL !");
+    ASSERT_MSG(string_memory_size > 0, "string_memory_size is 0 !");
+
+    size_t next_free_byte   = 0;                        // Naechstes freies Zeichen im Speicher
+    size_t remaining_memory = string_memory_size - 1;   // Noch freie Zeichen im Speicher
+    size_t used_char        = 0;                        // Anzahl an Zeichen, die im aktuellen snprintf-Aufruf in den
+                                                        // Speicher geschrieben wurde
+
+    if (remaining_memory == 0) { goto no_remaining_memory; }
+    used_char = (size_t) snprintf (string_memory + next_free_byte, remaining_memory, "(");
+    next_free_byte += used_char;
+    remaining_memory -= used_char;
+
+    // Astinhalt in die Zeichenkettendarstellung einbinden
+    for (uint_fast8_t i = 0; i < branch->length; ++ i)
+    {
+        if (remaining_memory == 0) { goto no_remaining_memory; }
+        used_char = (size_t) snprintf (string_memory + next_free_byte, remaining_memory, "%" PRIuFAST8, branch->branch [i]);
+        next_free_byte += used_char;
+        remaining_memory -= used_char;
+
+        if ((i + 1) < branch->length)
+        {
+            if (remaining_memory == 0) { goto no_remaining_memory; }
+            used_char = (size_t) snprintf (string_memory + next_free_byte, remaining_memory, ", ");
+            next_free_byte += used_char;
+            remaining_memory -= used_char;
+        }
+    }
+
+    if (remaining_memory == 0) { goto no_remaining_memory; }
+    used_char = (size_t) snprintf (string_memory + next_free_byte, remaining_memory, ")\n");
+    next_free_byte += used_char;
+    remaining_memory -= used_char;
+
+    // Laenge des Astes in die Zeichenkettendarstellung einbinden
+    if (remaining_memory == 0) { goto no_remaining_memory; }
+    used_char = (size_t) snprintf (string_memory + next_free_byte, remaining_memory, "Length: %" PRIuFAST8 "\n", branch->length);
+    next_free_byte += used_char;
+    remaining_memory -= used_char;
+
+    if (remaining_memory == 0) { goto no_remaining_memory; }
+    used_char = (size_t) snprintf (string_memory + next_free_byte, remaining_memory, "State: ");
+    next_free_byte += used_char;
+    remaining_memory -= used_char;
+
+    // Status in die Zeichenkettendarstellung einbringen
+    if (remaining_memory == 0) { goto no_remaining_memory; }
+    switch (branch->state)
+    {
+    case ALKANE_BRANCH_CREATED:
+        snprintf (string_memory + next_free_byte, remaining_memory, "ALKANE_BRANCH_CREATED\n");
+        break;
+    case ALKANE_BRANCH_DELETED:
+        snprintf (string_memory + next_free_byte, remaining_memory, "ALKANE_BRANCH_DELETED\n");
+        break;
+    case ALKANE_BRANCH_INITIALIZED_WITH_DATA:
+        snprintf (string_memory + next_free_byte, remaining_memory, "ALKANE_BRANCH_INITIALIZED_WITH_DATA\n");
+        break;
+    case ALKANE_BRANCH_INITIALIZED_WITH_ZERO_BYTES:
+        snprintf (string_memory + next_free_byte, remaining_memory, "ALKANE_BRANCH_INITIALIZED_WITH_ZERO_BYTES\n");
+        break;
+    case ALKANE_BRANCH_INVALID_BRANCH:
+        snprintf (string_memory + next_free_byte, remaining_memory, "ALKANE_BRANCH_INVALID_BRANCH\n");
+        break;
+    case ALKANE_BRANCH_UNKNOWN_STATE:
+        snprintf (string_memory + next_free_byte, remaining_memory, "ALKANE_BRANCH_UNKNOWN_STATE\n");
+        break;
+
+        // Der default-Pfad soll nie ausgefuehrt werden ! Wenn dies dennoch der Fall ist, dann ist dies ein Fehler !
+    default:
+        ASSERT_MSG(false, "default path of a switch case statement executed !");
+    }
+
+    no_remaining_memory:
+    string_memory [string_memory_size - 1] = '\0';  // Nullterminierung garantieren
+
+    return string_memory;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+/**
  * Alkane_Branch Objekt loeschen.
  * Da es sich bei dem Objekt um dynamischen Speicher handelt, muss sichergestellt werden, dass alle Objekte, die zuvor
  * erstellt wurden, geloescht werden. Nach dem Loeschen sollte der verwendete Zeiger nicht mehr dereferenziert werden !
