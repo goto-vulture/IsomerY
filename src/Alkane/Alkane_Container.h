19d6f5e5d21f666692b59f6c76d966e7f854c90c
Gyps <N/A>
2021-03-10 21:21:59 +0100

    Alkane_Container.h created.

---
+++ src/Alkane/Alkane_Container.h
@@ -0,0 +1,13 @@
+/**
+ * Alkane_Container.h
+ *
+ *  Created on: 10.03.2021
+ *      Author: x86 / Gyps
+ */
+
+#ifndef ALKANE_CONTAINER_H
+#define ALKANE_CONTAINER_H
+
+
+
+#endif /* ALKANE_CONTAINER_H */
