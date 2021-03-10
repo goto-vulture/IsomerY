3a7ceeebb329fa4d781839b06753a5ad7482a3a5
Gyps <N/A>
2021-03-10 19:16:03 +0100

    Function prototypes for the Alkane struct added.

--- src/Alkane/Alkane.h
+++ src/Alkane/Alkane.h
@@ -63,13 +63,101 @@ struct Alkane
 {
     struct Alkane_Branch* branches [4];                         // Die Alkane_Branch-Objekte
     uint_fast8_t next_free_branch;                              // Index des naechsten freien Alkane_Branch Zeigers
-    uint_fast8_t number_of_c_atoms;                             // Anzahl an C-Atomen im kompletten Alkan
 
     unsigned char merged_numbercode [MAX_NUMBER_OF_C_ATOMS];    // Zusammengefuehrter Zahlencode
                                                                 // Hiermit wird die genaue Struktur des Alkans bestimmt
+    uint_fast8_t number_of_c_atoms;                             // Anzahl an C-Atomen im kompletten Alkan
 
     enum Alkane_State state;                                    // Status des Objektes
 };
+
+//=====================================================================================================================
+
+/**
+ * Alkane Objekt dynamisch erzeugen.
+ *
+ * Alle Zeiger duerfen NULL sein, in diesem Fall wird davon ausgegangen, dass dieser Ast nur aus einem H-Atom besteht.
+ * Damit die Adressen, die ungleich NULL sind, aufsteigend im Alkane-Objekt gespeichert werden, (dies vereinfacht die
+ * weiteren Operationen) werden die uebergebenen Adressen im Objekt neu angeordnet, wenn die Alkane_Branch-Objekte
+ * nicht "von links nach rechts" der Funktion uebergeben werden.
+ *
+ * Asserts:
+ *      N/A
+ */
+extern struct Alkane*                               // Erzeugtes Alkane-Objekt
+Create_Alkane
+(
+        const struct Alkane_Branch* const branch_1, // 1. Ast des neuen Alkane-Objektes
+        const struct Alkane_Branch* const branch_2, // 2. Ast des neuen Alkane-Objektes
+        const struct Alkane_Branch* const branch_3, // 3. Ast des neuen Alkane-Objektes
+        const struct Alkane_Branch* const branch_4  // 4. Ast des neuen Alkane-Objektes
+);
+
+/**
+ * Alkane Objekt in eine Zeichenkettendarstellung ueberfuehren. Dies ist insbesondere fuer debugging hilfreich.
+ *
+ * Der Speicher fuer die Zeichenkette muss vom Aufrufer allokiert werden !
+ *
+ * Branch 1:
+ * <To_String Darstellung des 1. Astes>
+ * Branch 2:
+ * <To_String Darstellung des 2. Astes>
+ * Branch 3:
+ * <To_String Darstellung des 3. Astes>
+ * Branch 4:
+ * <To_String Darstellung des 4. Astes>
+ *
+ * (<Aufbau des Alkans>)
+ *
+ * Length: <Laenge>
+ * State: <Status>
+ *
+ * Asserts:
+ *      alkane != NULL,
+ *      string_memory != NULL,
+ *      string_memory_size > 0
+ */
+extern const char*                          // Adresse der Eingabe-Zeichenkette
+Alkane_To_String
+(
+        const struct Alkane* const branch,  // Alkane der als Zeichenkette dargestellt werden soll
+        char* const string_memory,          // Speicher, der fuer die Zeichenkettenerzeugung verwendet werden soll
+                                            // Der Speicher muss vorher vom Aufrufer allokiert werden
+        const size_t string_memory_size     // Groesse des Zeichenkettenspeichers
+                                            // Wenn der Speicher nicht ausreicht, dann wird am vorletzten Zeichen die
+                                            // Zeichenkette abgeschnitten
+);
+
+/**
+ * AlkaneObjekt auf stdout ausgeben. Hauptsaechlich fuer das Debbuging. Fuer die Konvertierung wird die Funktion
+ * "Alkane_To_String" verwendet.
+ *
+ * Asserts:
+ *      alkane != NULL
+ */
+extern void
+Print_Alkane
+(
+        const struct Alkane* const alkane   // Alkane, welches ausgegeben werden soll
+);
+
+/**
+ * Alkane Objekt loeschen.
+ *
+ * Die enthaltenen Alkane_Branch-Objekte werden NICHT geloescht, wenn das Alkane-Objekt geloescht wird. Es werden
+ * lediglich die Zeiger des Objektes auf NULL gesetzt !
+ *
+ * Da es sich bei dem Objekt um dynamischen Speicher handelt, muss sichergestellt werden, dass alle Objekte, die zuvor
+ * erstellt wurden, geloescht werden. Nach dem Loeschen sollte der verwendete Zeiger nicht mehr dereferenziert werden !
+ *
+ * Asserts:
+ *      alkane != NULL
+ */
+extern void
+Delete_Alkane
+(
+        struct Alkane* alkane   // Alkane, welches geloescht werden soll
+);
 
 
 
