1c8df5e75c78de3f119ad80d5117cb1f93b02ee0
Gyps <N/A>
2021-03-10 21:23:26 +0100

    C++ compability code added.

--- src/Alkane/Alkane_Container.h
+++ src/Alkane/Alkane_Container.h
@@ -8,6 +8,21 @@
 #ifndef ALKANE_CONTAINER_H
 #define ALKANE_CONTAINER_H
 
+// BEGINN C++-Kompablitaet herstellen
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
 
+
+
+
+
+
+
+// ENDE C++-Kompablitaet herstellen
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* ALKANE_CONTAINER_H */
