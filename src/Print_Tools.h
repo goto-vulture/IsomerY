dd5711c72098596a23935c1e7e015f3a73cf5c8a
Gyps <N/A>
2021-03-13 14:33:33 +0100

    Two print tool macros implemented.

--- src/Print_Tools.h
+++ src/Print_Tools.h
@@ -8,6 +8,45 @@
 #ifndef PRINT_TOOLS_H
 #define PRINT_TOOLS_H
 
+// BEGINN C++-Kompablitaet herstellen
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
 
+
+
+#include <stdio.h>
+
+
+
+/**
+ * Ausgabe mittels printf, wobei automatisch stdout geleert wird.
+ * Dies dient dazu, dass eine Ausgabe direkt auf dem Bildschirm erscheint.
+ */
+#ifndef PRINTF_FFLUSH
+    #define PRINTF_FFLUSH(format_string, ...)                   \
+    printf (format_string, __VA_ARGS__); fflush (stdout);
+#else
+    #error "The macro \"PRINTF_FFLUSH\" is already defined !"
+#endif /* PRINTF_FFLUSH */
+
+/**
+ * Ausgabe mittels puts, wobei automatisch stdout geleert wird.
+ * Dies dient dazu, dass eine Ausgabe direkt auf dem Bildschirm erscheint.
+ */
+#ifndef PUTS_FFLUSH
+    #define PUTS_FFLUSH(string)                                 \
+    puts (string); fflush (stdout);
+#else
+    #error "The macro \"PUTS_FFLUSH\" is already defined !"
+#endif /* PUTS_FFLUSH */
+
+
+
+// ENDE C++-Kompablitaet herstellen
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* PRINT_TOOLS_H */
