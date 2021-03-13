997c3f66309bc9899d0373e04a2905c2dc5de404
Gyps <N/A>
2021-03-13 20:24:54 +0100

    First alkane test implemented.

--- src/Tests/Alkane_Tests.c
+++ src/Tests/Alkane_Tests.c
@@ -6,3 +6,37 @@
  */
 
 #include "Alkane_Tests.h"
+#include "../Alkane/Alkane_Create_Constitutional_Isomer.h"
+#include "../Alkane/Alkane_Info_Constitutional_Isomer.h"
+
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Die Erstellung der Alkane testen. Vorerst nur ueber die Anzahl an Ergebnissen.
+ * Die Anzahl an Ergebnissen wird mit der Anzahl an moeglichen Konstitutionsisomeren verglichen. Wenn die Werte gleich
+ * sind, dann ist ein Fehler in der Berechnung relativ unwahrscheinlich.
+ *
+ * Die Liste an moeglichen Konstitutionsisomeren bis 40 sind in der Datei "Alkane_Info_Constitutional_Isomer.h" zu
+ * finden.
+ *
+ * Aus Laufzeitgruenden wird nur bis inklusive 12 getestet.
+ */
+void TEST_Create_Alkane_Constitutional_Isomers ()
+{
+    uint_fast64_t number_of_created_isomers = 0;
+
+    for (uint_fast8_t i = 3; i <= 12; ++ i)
+    {
+        Create_Alkane_Constitutional_Isomers (i, &number_of_created_isomers);
+
+        ASSERT_EQUALS(NUMBER_OF_ALKANE_CONSTITUTIONAL_ISOMER [i - 1], number_of_created_isomers);
+    }
+
+    return;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
--- src/Tests/Alkane_Tests.h
+++ src/Tests/Alkane_Tests.h
@@ -8,6 +8,35 @@
 #ifndef TESTS_ALKANE_TESTS_H
 #define TESTS_ALKANE_TESTS_H
 
+// BEGINN C++-Kompablitaet herstellen
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
 
+
+
+#include "tinytest.h"
+
+
+
+/**
+ * Die Erstellung der Alkane testen. Vorerst nur ueber die Anzahl an Ergebnissen.
+ * Die Anzahl an Ergebnissen wird mit der Anzahl an moeglichen Konstitutionsisomeren verglichen. Wenn die Werte gleich
+ * sind, dann ist ein Fehler in der Berechnung relativ unwahrscheinlich.
+ *
+ * Die Liste an moeglichen Konstitutionsisomeren bis 40 sind in der Datei "Alkane_Info_Constitutional_Isomer.h" zu
+ * finden.
+ *
+ * Aus Laufzeitgruenden wird nur bis inklusive 12 getestet.
+ */
+extern void TEST_Create_Alkane_Constitutional_Isomers ();
+
+
+
+// ENDE C++-Kompablitaet herstellen
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* TESTS_ALKANE_TESTS_H */
