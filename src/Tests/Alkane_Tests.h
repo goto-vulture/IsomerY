b22f83f4d5ab66dcb3eba02f171fccac0dce8f8e
Gyps <N/A>
2021-03-13 19:35:01 +0100

    Alkane_Tests.h created.

---
+++ src/Tests/Alkane_Tests.h
@@ -0,0 +1,13 @@
+/**
+ * Alkane_Tests.h
+ *
+ *  Created on: 13.03.2021
+ *      Author: x86 / Gyps
+ */
+
+#ifndef TESTS_ALKANE_TESTS_H
+#define TESTS_ALKANE_TESTS_H
+
+
+
+#endif /* TESTS_ALKANE_TESTS_H */
