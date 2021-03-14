721ef167d124deb863c2fa31413c32dcea3202bd
Gyps <N/A>
2021-03-14 20:24:55 +0100

    Alkane_To_IUPAC_Name.h created.

---
+++ src/Alkane/Alkane_To_IUPAC_Name.h
@@ -0,0 +1,13 @@
+/**
+ * Alkane_To_IUPAC_Name.h
+ *
+ *  Created on: 14.03.2021
+ *      Author: x86 / Gyps
+ */
+
+#ifndef ALKANE_TO_IUPAC_NAME_H
+#define ALKANE_TO_IUPAC_NAME_H
+
+
+
+#endif /* ALKANE_TO_IUPAC_NAME_H */
