d60423012f14e774b2b0a27dca7ce922ddd6f6c5
Gyps <N/A>
2021-03-14 17:23:45 +0100

    static_asserts were moved to the file "OS_Specific_Configurations.h".

--- src/OS_Specific_Configurations.h
+++ src/OS_Specific_Configurations.h
@@ -16,6 +16,40 @@ extern "C"
 
 
 
+#include <stdint.h>
+#include <limits.h>
+#include <time.h>
+
+
+
+/**
+ * Die static_asserts funktionieren aktuell nur unter Linux
+ */
+#if defined(__linux__) || defined(unix) || defined(__unix__) || defined(__unix)
+// Ist auf der Zielplattform ein Byte genau 8 Bit gross ?
+// Wenn nicht dann wird die Kompilierung nicht ausgefuehrt, da alle verwendeten Mechanismen davon ausgehen das dies der
+// Fall ist !
+static_assert (CHAR_BIT == 8,               "A char needs to be exact eight bit !");
+
+// Ein int / unsigned int / size_t muss mindestens 4 Byte gross sein ! Bei Systemen wo dies nicht der Fall ist, ist
+// i.d.R. ohne groesseren Aufwand kaum zu arbeiten.
+static_assert (sizeof (int) >= 4,           "The type \"int\" needs to be at least 4 byte !");
+static_assert (sizeof (unsigned int) >= 4,  "The type \"unsigned int\" needs to be at least 4 byte !");
+static_assert (sizeof (size_t) >= 4,        "The type \"size_t\" needs to be at least 4 byte !");
+
+// Weitere Variable(n), die mindestens 4 Byte gross sein sollte(n)
+static_assert (sizeof (time_t) >= 4,        "The type \"time_t\" needs to be at least 4 byte !");
+static_assert (sizeof (clock_t) >= 4,       "The type \"clock_t\" needs to be at least 4 byte !");
+
+// Der Standard schreibt vor, dass SIZE_MAX mindestens 65535 betragen muss. Fuer einige Operationen (insb. calloc (),
+// malloc () und Co.) ist dies hier aber zu wenig ! Daher muss sichergestellt sein, dass SIZE_MAX eine akzeptable
+// Groesse besitzt.
+// I.d.R. ist SIZE_MAX (2^32) - 1 oder (2^64) - 1
+static_assert (SIZE_MAX >= UINT32_MAX,      "SIZE_MAX needs to be at least 2^32 - 1 !");
+#endif /* defined(__linux__) || defined(unix) || defined(__unix__) || defined(__unix) */
+
+
+
 /**
  * Konfigurationen, wenn man sich auf einem Windows-System befindet
  */
