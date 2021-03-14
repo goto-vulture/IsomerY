295dcdee01c659f234c74fb50cba26b26122e164
Gyps Win <N/A>
2021-03-14 17:07:55 +0100

    OS specific configurations extended to avoid compiler warnings.

--- src/OS_Specific_Configurations.h
+++ src/OS_Specific_Configurations.h
@@ -26,6 +26,31 @@ extern "C"
     #else
         #error "The macro \"WINDOWS\" is already defined !"
     #endif /* WINDOWS */
+
+    // Damit es unter Windows bei dem Formatstring fuer size_t zu keiner Warnung kommt, wird diese Konstruktion
+    // verwendet. PRI_SIZET wird mit einem MinGW spezifischen Formatstring ersetzt, der die passende Groesse fuer
+    // ein size_t Objekt bereitstellt.
+    #ifdef _WIN64
+        #ifndef PRI_SIZET
+            #define PRI_SIZET PRIu64
+        #else
+            #error "The macro \"PRI_SIZET\" is already defined !"
+        #endif /* PRI_SIZET */
+    #else
+        #ifndef PRI_SIZET
+            #define PRI_SIZET PRIu32
+        #else
+            #error "The macro \"PRI_SIZET\" is already defined !"
+        #endif /* PRI_SIZET */
+    #endif
+#else
+    // Unter anderen Betriebssystemen ist kein spezieller Formatstring fuer size_t notwendig, also wird der normale
+    // (zu) verwendet.
+    #ifndef PRI_SIZET
+        #define PRI_SIZET "zu"
+    #else
+        #error "The macro \"PRI_SIZET\" is already defined !"
+    #endif /* PRI_SIZET */
 #endif /* defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) */
 
 
