8a30d2f944d7dc80b12ef819ebe0451092d373a5
Gyps <N/A>
2021-03-15 21:09:28 +0100

    Empty function implementation added.

--- src/Alkane/Alkane_To_IUPAC_Name.c
+++ src/Alkane/Alkane_To_IUPAC_Name.c
@@ -6,3 +6,30 @@
  */
 
 #include "Alkane_To_IUPAC_Name.h"
+#include "../Error_Handling/Assert_Msg.h"
+
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * IUPAC-Name des uebergebenen Alkans erzeugen.
+ *
+ * ...
+ *
+ * Asserts:
+ *      alkane != NULL
+ */
+void Convert_Alkane_To_IUPAC_Name
+(
+        struct Alkane* const alkane         // Alkan, von dem der IUPAC-Name gebildet werden soll
+)
+{
+    ASSERT_MSG(alkane != NULL, "alkane is NULL !");
+
+    return;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
