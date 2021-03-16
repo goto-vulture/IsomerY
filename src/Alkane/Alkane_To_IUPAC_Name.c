8404a65885aab2a4a9df49d660c125c6ac323e76
Gyps <N/A>
2021-03-16 10:44:55 +0100

    Numbercode check added.

--- src/Alkane/Alkane_To_IUPAC_Name.c
+++ src/Alkane/Alkane_To_IUPAC_Name.c
@@ -6,6 +6,7 @@
  */
 
 #include "Alkane_To_IUPAC_Name.h"
+#include <string.h>
 #include "../Error_Handling/Assert_Msg.h"
 
 
@@ -28,6 +29,29 @@ void Convert_Alkane_To_IUPAC_Name
 )
 {
     ASSERT_MSG(alkane != NULL, "alkane is NULL !");
+
+    // ===== BEGINN Zusammengesetzten Zahlencode des uebergebenen Alkans ueberpruefen =====
+    uint_fast8_t count_number_occur [MAX_NUMBER_OF_C_ATOMS];
+    memset (count_number_occur, '\0', sizeof (count_number_occur));
+
+    for (uint_fast8_t i = 0; i < alkane->number_of_c_atoms; ++ i)
+    {
+        count_number_occur [alkane->merged_numbercode [i] - 1] ++;
+    }
+    for (uint_fast8_t i = 0; i < alkane->number_of_c_atoms; ++ i)
+    {
+        if (i == 0)
+        {
+            ASSERT_FMSG(count_number_occur [0] <= 5, "Invalid merged numbercode ! The number %" PRIdFAST8 " occurs %"
+                    PRIdFAST8 " times ! Max. valid: 5 !", (uint_fast8_t) (i + 1), count_number_occur [0]);
+        }
+        else
+        {
+            ASSERT_FMSG(count_number_occur [i] <= 4, "Invalid merged numbercode ! The number %" PRIdFAST8 " occurs %"
+                    PRIdFAST8 " times ! Max. valid: 4 !", (uint_fast8_t) (i + 1), count_number_occur [i]);
+        }
+    }
+    // ===== ENDE Zusammengesetzten Zahlencode des uebergebenen Alkans ueberpruefen =====
 
     return;
 }
