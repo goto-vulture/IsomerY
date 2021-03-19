12a02acbcbceb024889a7c42fac6c7f0ca726557
Gyps <N/A>
2021-03-19 17:10:15 +0100

    Typo in a comment corrected.

--- src/Alkane/Alkane_To_IUPAC_Name.c
+++ src/Alkane/Alkane_To_IUPAC_Name.c
@@ -120,7 +120,7 @@ void Convert_Alkane_To_IUPAC_Name
     {
         const unsigned char current_numercode_element   = alkane->merged_numbercode [current_c_atom];
 
-        // Adjazenzmatrix ist symmetrisch dir Diagonalen, da die Bindungen zwischen zwei C-Atomen ungerichtet ist
+        // Adjazenzmatrix ist symmetrisch zur Diagonalen, da die Bindungen zwischen zwei C-Atomen ungerichtet sind
         alkane->structure [current_c_atom][current_numercode_element - 1] = 1;
         alkane->structure [current_numercode_element - 1][current_c_atom] = 1;
     }
