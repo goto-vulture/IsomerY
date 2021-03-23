7179a801dff69d9a45fedb9eaa4199347a3cfd65
Gyps <N/A>
2021-03-23 16:26:09 +0100

    Alkane_Chain_To_IUPAC_Name.h created.

---
+++ src/Alkane/Alkane_Chain_To_IUPAC_Name.h
@@ -0,0 +1,13 @@
+/**
+ * Alkane_Chain_To_IUPAC_Name.h
+ *
+ *  Created on: 23.03.2021
+ *      Author: x86 / Gyps
+ */
+
+#ifndef ALKANE_CHAIN_TO_IUPAC_NAME_H
+#define ALKANE_CHAIN_TO_IUPAC_NAME_H
+
+
+
+#endif /* ALKANE_CHAIN_TO_IUPAC_NAME_H */
