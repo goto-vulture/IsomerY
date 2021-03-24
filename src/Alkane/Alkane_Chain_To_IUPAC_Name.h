0b19c4ee24d5f05885fd076d7f2868e82aee7934
Gyps <N/A>
2021-03-24 20:56:02 +0100

    Alkane_Chain_To_IUPAC_Name.h implemented.

--- src/Alkane/Alkane_Chain_To_IUPAC_Name.h
+++ src/Alkane/Alkane_Chain_To_IUPAC_Name.h
@@ -8,6 +8,31 @@
 #ifndef ALKANE_CHAIN_TO_IUPAC_NAME_H
 #define ALKANE_CHAIN_TO_IUPAC_NAME_H
 
+// BEGINN C++-Kompablitaet herstellen
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
 
+
+
+#include "Alkane_To_IUPAC_Name.h"
+
+
+
+extern void
+Chain_To_IUPAC
+(
+        char* iupac_name,
+        size_t iupac_name_length,
+        struct Alkane* alkane
+);
+
+
+
+// ENDE C++-Kompablitaet herstellen
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* ALKANE_CHAIN_TO_IUPAC_NAME_H */
