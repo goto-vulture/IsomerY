5b287def56c2ec48e780db856185e04dd304c62e
Gyps <N/A>
2021-12-09 16:26:48 +0100

    Doxygen comments updated.

--- src/str2int.h
+++ src/str2int.h
@@ -55,11 +55,13 @@ extern enum str2int_errno str2int (long int* out, const char* input_string, cons
 /**
  * @brief long int zu C-String konvertieren.
  *
- * ...
+ * Fehlerhafte Eingaben werden ueber den Rueckgabewert angezeigt.
  *
  * @param[out] output_string C-String fuer die Ausgabe
  * @param[in] output_string_size Maximale Groesse des Ausgabestrings.
  * @param[in] input Integer, der konvertiert wird.
+ *
+ * @return Fehlercode. 0, wenn alles in Ordnung war; ansonsten != 0
  */
 extern enum int2str_errno int2str (char* const output_string, const size_t output_string_size, const long int input);
 
