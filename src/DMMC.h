3bd6bb9326efc45cff8774b8b8044ecde8af4285
Gyps <N/A>
2022-01-17 20:36:36 +0100

    Code formatting.

--- src/DMMC.h
+++ src/DMMC.h
@@ -42,12 +42,12 @@ extern "C"
  * @brief Container mit allen Funktionen sowie die notwendige Strukturdefinition erzeugen.
  */
 #ifndef CREATE_CONTAINER
-#define CREATE_CONTAINER(object_type, object_type_name) \
-    CREATE_CONTAINER_STRUCT(object_type, object_type_name) \
-    CREATE_CREATE_CONTAINER_FUNCTION(object_type, object_type_name) \
-    CREATE_DELETE_CONTAINER_FUNCTION(object_type_name) \
-    CREATE_GET_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name) \
-    CREATE_SET_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name) \
+#define CREATE_CONTAINER(object_type, object_type_name)                     \
+    CREATE_CONTAINER_STRUCT(object_type, object_type_name)                  \
+    CREATE_CREATE_CONTAINER_FUNCTION(object_type, object_type_name)         \
+    CREATE_DELETE_CONTAINER_FUNCTION(object_type_name)                      \
+    CREATE_GET_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name)    \
+    CREATE_SET_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name)    \
     CREATE_APPEND_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name)
 #else
 #error "The macro \"CREATE_CONTAINER\" is already defined !"
@@ -59,14 +59,14 @@ extern "C"
  * @brief Strukturdefinition fuer den dynamischen Container erzeugen. Hierbei ist der Objekt-Typ ein Makroparameter.
  */
 #ifndef CREATE_CONTAINER_STRUCT
-#define CREATE_CONTAINER_STRUCT(object_type, object_type_name) \
-    struct object_type_name ## _container \
-{ \
-    object_type* data; \
-    \
-    size_t one_object_size; \
-    uint_fast64_t container_size; \
-    uint_fast64_t next_free_element; \
+#define CREATE_CONTAINER_STRUCT(object_type, object_type_name)  \
+    struct object_type_name ## _container                       \
+{                                                               \
+    object_type* data;                                          \
+                                                                \
+    size_t one_object_size;                                     \
+    uint_fast64_t container_size;                               \
+    uint_fast64_t next_free_element;                            \
 };
 #else
 #error "The macro \"CREATE_CONTAINER_STRUCT\" is already defined !"
@@ -78,25 +78,22 @@ extern "C"
  * @brief Erzeugungsfunktion fuer den dynamischen Container mittels eines Makros erzeugen.
  */
 #ifndef CREATE_CREATE_CONTAINER_FUNCTION
-#define CREATE_CREATE_CONTAINER_FUNCTION(object_type, object_type_name) \
-        struct object_type_name ## _container* Create_ ## object_type_name ## _Container(void); \
-        struct object_type_name ## _container* Create_ ## object_type_name ## _Container(void)\
-        { \
-            struct object_type_name ## _container* new_container = \
-                    (struct object_type_name ## _container*) CALLOC (1, sizeof(struct object_type_name ## _container)); \
-            if (new_container == NULL) \
-            { \
-                return NULL; \
-            } \
-            \
-            new_container->one_object_size = sizeof(object_type); \
-            \
-            new_container->data = (object_type*) CALLOC (ALLOCATION_STEP_SIZE, sizeof(object_type)); \
-            ASSERT_ALLOC(new_container->data, "Try to create a " #object_type_name " container.", \
-                    ALLOCATION_STEP_SIZE * sizeof(object_type))\
-            new_container->container_size = ALLOCATION_STEP_SIZE; \
-            return new_container; \
-}
+#define CREATE_CREATE_CONTAINER_FUNCTION(object_type, object_type_name)                                             \
+    struct object_type_name ## _container* Create_ ## object_type_name ## _Container(void);                         \
+    struct object_type_name ## _container* Create_ ## object_type_name ## _Container(void)                          \
+    {                                                                                                               \
+        struct object_type_name ## _container* new_container =                                                      \
+                (struct object_type_name ## _container*) CALLOC (1, sizeof(struct object_type_name ## _container)); \
+        if (new_container == NULL) { return NULL; }                                                                 \
+                                                                                                                    \
+        new_container->one_object_size = sizeof(object_type);                                                       \
+                                                                                                                    \
+        new_container->data = (object_type*) CALLOC (ALLOCATION_STEP_SIZE, sizeof(object_type));                    \
+        ASSERT_ALLOC(new_container->data, "Try to create a " #object_type_name " container.",                       \
+                ALLOCATION_STEP_SIZE * sizeof(object_type))                                                         \
+        new_container->container_size = ALLOCATION_STEP_SIZE;                                                       \
+        return new_container;                                                                                       \
+    }
 #else
 #error "The macro \"CREATE_CREATE_CONTAINER_FUNCTION\" is already defined !"
 #endif /* CREATE_CREATE_CONTAINER_FUNCTION */
@@ -107,20 +104,15 @@ extern "C"
  * @brief Loeschfunktion fuer den dynamischen Container erzeugen. Hierbei wird ein Praeprozessormakro verwendet.
  */
 #ifndef CREATE_DELETE_CONTAINER_FUNCTION
-#define CREATE_DELETE_CONTAINER_FUNCTION(object_type_name) \
-        void Delete_ ## object_type_name ## _Container(struct object_type_name ## _container* container); \
-    void Delete_ ## object_type_name ## _Container(struct object_type_name ## _container* container) \
-    { \
-    if (container == NULL) \
-    { \
-        return; \
-    } \
-    if (container->data != NULL) \
-    { \
-        FREE_AND_SET_TO_NULL (container->data); \
-    } \
-    FREE_AND_SET_TO_NULL (container); \
-    return; \
+#define CREATE_DELETE_CONTAINER_FUNCTION(object_type_name)                                              \
+    void Delete_ ## object_type_name ## _Container(struct object_type_name ## _container* container);   \
+    void Delete_ ## object_type_name ## _Container(struct object_type_name ## _container* container)    \
+    {                                                                                                   \
+        if (container == NULL) { return; }                                                              \
+        if (container->data != NULL) { FREE_AND_SET_TO_NULL (container->data); }                        \
+                                                                                                        \
+        FREE_AND_SET_TO_NULL (container);                                                               \
+        return;                                                                                         \
     }
 #else
 #error "The macro \"CREATE_DELETE_CONTAINER_FUNCTION\" is already defined !"
@@ -134,14 +126,14 @@ extern "C"
  * Die Get-Funktion des Containers gibt das Objekt an der Stelle des uebergebenen Indexes zurueck.
  */
 #ifndef CREATE_GET_ELEMENT_CONTAINER_FUNCTION
-#define CREATE_GET_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name) \
-    object_type Get_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container, \
-            uint_fast64_t index); \
-    object_type Get_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container, \
-            uint_fast64_t index) \
-        { \
-    return container->data[index]; \
-        }
+#define CREATE_GET_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name)                                        \
+    object_type Get_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container,    \
+            uint_fast64_t index);                                                                                   \
+    object_type Get_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container,    \
+    uint_fast64_t index)                                                                                            \
+    {                                                                                                               \
+        return container->data[index];                                                                              \
+    }
 #else
 #error "The macro \"CREATE_GET_ELEMENT_CONTAINER_FUNCTION\" is already defined !"
 #endif /* CREATE_GET_ELEMENT_CONTAINER_FUNCTION */
@@ -158,25 +150,24 @@ extern "C"
  * Zaehler der Append-Funktion durch die Set-Funktion nicht angepasst wird.
  */
 #ifndef CREATE_SET_ELEMENT_CONTAINER_FUNCTION
-#define CREATE_SET_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name) \
-    void Set_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container, \
-            object_type element, uint_fast64_t index); \
-    void Set_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container, \
-            object_type element, uint_fast64_t index) \
-        { \
-    \
-    if (container->next_free_element >= container->container_size) \
-    { \
-        container->container_size += ALLOCATION_STEP_SIZE; \
-        container->data = (object_type*) REALLOC (container->data, container->container_size * container->one_object_size); \
-        ASSERT_ALLOC(container->data, "Try to reallocate the memory for a/an " #object_type_name " container.", \
-                container->container_size * container->one_object_size) \
-        memset(container->data + (container->container_size - ALLOCATION_STEP_SIZE), '\0', \
-                ALLOCATION_STEP_SIZE * container->one_object_size); \
-    } \
-    container->data [index] = element; \
-    return; \
-        }
+#define CREATE_SET_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name)                                                    \
+    void Set_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container,                       \
+            object_type element, uint_fast64_t index);                                                                          \
+    void Set_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container,                       \
+            object_type element, uint_fast64_t index)                                                                           \
+    {                                                                                                                           \
+        if (container->next_free_element >= container->container_size)                                                          \
+        {                                                                                                                       \
+            container->container_size += ALLOCATION_STEP_SIZE;                                                                  \
+            container->data = (object_type*) REALLOC (container->data, container->container_size * container->one_object_size); \
+            ASSERT_ALLOC(container->data, "Try to reallocate the memory for a/an " #object_type_name " container.",             \
+                    container->container_size * container->one_object_size)                                                     \
+            memset(container->data + (container->container_size - ALLOCATION_STEP_SIZE), '\0',                                  \
+                    ALLOCATION_STEP_SIZE * container->one_object_size);                                                         \
+        }                                                                                                                       \
+        container->data [index] = element;                                                                                      \
+        return;                                                                                                                 \
+    }
 #else
 #error "The macro \"CREATE_SET_ELEMENT_CONTAINER_FUNCTION\" is already defined !"
 #endif /* CREATE_SET_ELEMENT_CONTAINER_FUNCTION */
@@ -194,26 +185,25 @@ extern "C"
  * Zaehler der Append-Funktion durch die Set-Funktion nicht angepasst wird.
  */
 #ifndef CREATE_APPEND_ELEMENT_CONTAINER_FUNCTION
-#define CREATE_APPEND_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name) \
-    void Append_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container, \
-            object_type element); \
-    void Append_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container, \
-            object_type element) \
-        { \
-    \
-    if (container->next_free_element >= container->container_size) \
-    { \
-        container->container_size += ALLOCATION_STEP_SIZE; \
-        container->data = (object_type*) REALLOC (container->data, container->container_size * container->one_object_size); \
-        ASSERT_ALLOC(container->data, "Try to reallocate the memory for a/an " #object_type_name " container.", \
-                container->container_size * container->one_object_size) \
-        memset(container->data + container->next_free_element, '\0', \
-                ALLOCATION_STEP_SIZE * container->one_object_size); \
-    } \
-    container->data [container->next_free_element] = element; \
-    container->next_free_element ++; \
-    return; \
-        }
+#define CREATE_APPEND_ELEMENT_CONTAINER_FUNCTION(object_type, object_type_name)                                                 \
+    void Append_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container,                    \
+            object_type element);                                                                                               \
+    void Append_ ## object_type_name ## _Container_Element(struct object_type_name ## _container* container,                    \
+            object_type element)                                                                                                \
+    {                                                                                                                           \
+        if (container->next_free_element >= container->container_size)                                                          \
+        {                                                                                                                       \
+            container->container_size += ALLOCATION_STEP_SIZE;                                                                  \
+            container->data = (object_type*) REALLOC (container->data, container->container_size * container->one_object_size); \
+            ASSERT_ALLOC(container->data, "Try to reallocate the memory for a/an " #object_type_name " container.",             \
+                    container->container_size * container->one_object_size)                                                     \
+            memset(container->data + container->next_free_element, '\0',                                                        \
+                    ALLOCATION_STEP_SIZE * container->one_object_size);                                                         \
+        }                                                                                                                       \
+        container->data [container->next_free_element] = element;                                                               \
+        container->next_free_element ++;                                                                                        \
+        return;                                                                                                                 \
+    }
 #else
 #error "The macro \"CREATE_APPEND_ELEMENT_CONTAINER_FUNCTION\" is already defined !"
 #endif /* CREATE_APPEND_ELEMENT_CONTAINER_FUNCTION */
