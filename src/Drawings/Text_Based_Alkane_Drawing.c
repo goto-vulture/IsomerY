99cfb4454177c03ff0046a2059cea711967e79b2
T430 <N/A>
2022-03-02 20:51:23 +0100

    Add create and delete function (w/o converting algorithm).

--- src/Drawings/Text_Based_Alkane_Drawing.c
+++ src/Drawings/Text_Based_Alkane_Drawing.c
@@ -8,3 +8,89 @@
  */
 
 #include "Text_Based_Alkane_Drawing.h"
+
+
+
+//---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * @brief Eine text-basierte Zeichnung eines Alkans erstellen.
+ *
+ * Grundsaetziches Vorgehen:
+ * - Aufspaltung des Namens an bestimmten Minuszeichnen mithilfe des Chain-Lexers, der urspruenglich fuer die
+ *   Testfunktionen geschrieben wurde
+ *
+ * - Ermittlung der tiefsten Verschachtelung
+ *
+ * - Zeichnung der Hauptkette in der mittleren Zeile (Anhand der tiefsten Verschachtelung wird entschieden wie gross
+ *   der Abstand zwischen den C-Atomen sein wird)
+ *
+ * - Zeichnung aller Aeste, die sich direkt an der Hauptkette befinden und keine Verschachtelung besitzen
+ *
+ * - Zeichnung aller Aeste, die eine Verschachtelung besitzen. Dabei wird "von Oben nach Unten" die Zeichnung erzeugt.
+ *   Stufenweise wird in die Tiefe der Verschachtelung gegangen.
+ *
+ * Asserts:
+ *      iupac_name != NULL
+ *      name_length > 0
+ *      name_length <= IUPAC_NAME_LENGTH
+ *
+ * @param[in] iupac_name IUPAC-Name eines Alkans
+ * @param[in] name_length Laenge des IUPAC-Namen
+ */
+extern struct Text_Based_Alkane_Drawing*        // Das Ergebnis (die textbasierte-Zeichnung)
+Create_Text_Based_Alkane_Drawing
+(
+        const char* const restrict iupac_name,  // IUPAC-Name, der gezeichnet werden soll
+        const size_t name_length                // Laenge des IUPAC-Namen
+)
+{
+    ASSERT_MSG(iupac_name != NULL,                  "IUPAC name is NULL !");
+    ASSERT_MSG(name_length > 0,                     "name_length is 0 !");
+    ASSERT_FMSG(name_length <= IUPAC_NAME_LENGTH,   "name_lenght is invalid ! Valid range: %u - %u.",
+            1u, IUPAC_NAME_LENGTH)
+
+
+    struct Text_Based_Alkane_Drawing* drawing = CALLOC(1, sizeof (struct Text_Based_Alkane_Drawing));
+    ASSERT_ALLOC(drawing, "Cannot allocate an Text_Based_Alkane_Drawing object !",
+            sizeof (struct Text_Based_Alkane_Drawing));
+
+    memcpy (drawing->iupac_name, iupac_name, name_length);
+
+    // "Zeichenflache" mit Leerzeichen fuellen (+ Terminatorsymbol)
+    for (size_t i = 0; i < TEXT_BASED_ALKANE_DRAWING_DIM_1; ++ i)
+    {
+        memset(drawing->drawing [i], ' ', sizeof (char) * (TEXT_BASED_ALKANE_DRAWING_DIM_2 - 1));
+        drawing->drawing [i][TEXT_BASED_ALKANE_DRAWING_DIM_2 - 1] = '\0';
+    }
+
+    drawing->state = TEXT_BASED_ALKANE_DRAWING_INITIALIZED_WITH_DATA;
+
+    return drawing;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * @brief Eine text-basierte Zeichnung komplett loeschen.
+ *
+ * Asserts:
+ *         alkane != NULL
+ *
+ * @param[in] text_based_drawing Text-Zeichnung, welches geloescht werden soll
+ */
+extern void
+Delete_Text_Based_Alkane_Drawing
+(
+        struct Text_Based_Alkane_Drawing* restrict text_based_drawing   // Objekt, welches geloescht werden soll
+)
+{
+    ASSERT_MSG(text_based_drawing != NULL, "drawing is NULL !");
+
+    text_based_drawing->state = TEXT_BASED_ALKANE_DRAWING_DELETED;
+    FREE_AND_SET_TO_NULL(text_based_drawing);
+
+    return;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
