6de34a7c5a67e0c02b4108526e7653345475e0f3
Gyps <N/A>
2022-03-10 14:43:41 +0100

    New translation unit for the alkane parser.

---
+++ src/Drawings/Alkane_Parser.c
@@ -0,0 +1,8 @@
+/**
+ * @file Alkane_Drawing.c
+ *
+ * @date 10.03.2022
+ * @author am1
+ */
+
+#include "Alkane_Parser.h"
---
+++ src/Drawings/Alkane_Parser.h
@@ -0,0 +1,29 @@
+/**
+ * @file Alkane_Parser.h
+ *
+ * @date 10.03.2022
+ * @author am1
+ */
+
+#ifndef ALKANE_PARSER_H
+#define ALKANE_PARSER_H ///< Include-Guard
+
+// BEGINN C++-Kompablitaet herstellen
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+
+
+//
+
+
+
+// ENDE C++-Kompablitaet herstellen
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+#endif /* ALKANE_PARSER_H */
