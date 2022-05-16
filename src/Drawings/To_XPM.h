3184a81113c50c3a6499fddd78c0ea5a40bf5680
Gyps <N/A>
2022-05-16 21:58:03 +0200

    First non 100% complete code for exporting text-based drawings to XPM files.

--- src/Drawings/To_XPM.h
+++ src/Drawings/To_XPM.h
@@ -22,7 +22,24 @@ extern "C"
 
 
 
-// ...
+#include "Text_Based_Alkane_Drawing.h"
+
+
+
+enum Export_To_XPM_Settings
+{
+    EXPORT_XPM_CHAR_SIZE_32_32 = 1 << 1,
+    EXPORT_XPM_CHAR_SIZE_64_64 = 1 << 2,
+
+    EXPORT_XPM_CHAR_PER_PIXEL_1 = 1 << 3,
+    EXPORT_XPM_CHAR_PER_PIXEL_2 = 1 << 4
+};
+
+extern void Export_Text_Based_Drawing_To_XPM
+(
+        const struct Text_Based_Alkane_Drawing* const restrict input,
+        const enum Export_To_XPM_Settings export_settings
+);
 
 
 
